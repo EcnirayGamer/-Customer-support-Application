4392eb8a4fe62cad7bb4f58b6543e69ed708e3de
EcnirayGamer <ymateo4605@gmail.com>
2019-03-21 16:38:33 -0400

    added database to Login

--- source/production/java/com/wrox/LoginServlet.java
+++ source/production/java/com/wrox/LoginServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.Hashtable;
 import java.util.Map;
+import java.sql.*;
 
 @WebServlet(
         name = "loginServlet",
@@ -20,11 +21,23 @@ public class LoginServlet extends HttpServlet
     private static final Map<String, String> userDatabase = new Hashtable<>();
 
     static {
-        userDatabase.put("Nicholas", "password");
-        userDatabase.put("Sarah", "drowssap");
-        userDatabase.put("Mike", "wordpass");
-        userDatabase.put("John", "green");
-    }
+        //userDatabase.put("Nicholas", "password");
+        //userDatabase.put("Sarah", "drowssap");
+        //userDatabase.put("Mike", "wordpass");
+        //userDatabase.put("John", "green");
+    	try{  
+    		Class.forName("com.mysql.jdbc.Driver");  
+    		Connection con=DriverManager.getConnection(  
+    		"jdbc:mysql://localhost:3306/customersupport","root","password");  
+    		//here sonoo is database name, root is username and password  
+    		Statement stmt=con.createStatement();  
+    		ResultSet rs=stmt.executeQuery("select * from User");  
+    		while(rs.next())  
+    		userDatabase.put(rs.getString(2),rs.getString(5));  
+    		con.close();  
+    		}catch(Exception e){ System.out.println(e);}  
+    		}  
+    
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -76,4 +89,6 @@ public class LoginServlet extends HttpServlet
             response.sendRedirect("tickets");
         }
     }
+     
+   
 }
